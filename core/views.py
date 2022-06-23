b7ba235fca05470e54190ce5a9ed8dc1fb818b99
RODRIGO GALLEGO <50471819+ragallegom@users.noreply.github.com>
2022-06-22 23:49:02 -0500

    import nltk

--- core/views.py
+++ core/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the core index")
+
+    print(request)
+    
+    example_string = "The friends of DeSoto love scarves."
+
+    words_in_quote = word_tokenize(example_string)
+
+    stop_words = set(stopwords.words("english"))
+
+    filtered_list = []
+
+    for word in words_in_quote:
+        if word.casefold() not in stop_words:
+                filtered_list.append(word)
+
+    filtered_list = [
+        word for word in words_in_quote if word.casefold() not in stop_words
+    ]
+
+    stemmer = PorterStemmer()
+    stemmed_words = [stemmer.stem(word) for word in filtered_list]
+
+    lemmatizer = WordNetLemmatizer()
+    lemmatizer.lemmatize("scarves")
+
+    lemmatized_words = [lemmatizer.lemmatize(word) for word in stemmed_words]
+
+    response_data = {}
+    response_data['result'] = lemmatized_words
+
+    return JsonResponse(response_data)
---
+++ ui/src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+  <form id="app" @submit="checkForm" action="http://localhost:8000/core/" method="post">
+    <p v-if="errors.length">
+      <b>Por favor, corrija el(los) siguiente(s) error(es):</b>
+    <ul>
+      {{ errors }}
+    </ul>
+    </p>
+    <p>
+      <label for="name">Texto</label><br>
+      <input id="name" v-model="name" type="text" name="name">
+    </p>
+    <p>
+      <input type="submit" value="Enviar">
+    </p>
+  </form>
+  /*******************************************/
+  <p>PROXIMAMENTE</p>
+  <p>Subir archivo:</p>
+  <input type="file" id="avatar" name="avatar" accept=".doc, .docx, .pdf">
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      message: '',
+      errors: [],
+      name: null
+    }
+  },
+  methods:{
+    checkForm: function (e) {
+      if (this.name) {
+        return true;
+      }
+
+      this.errors = [];
+
+      if (!this.name) {
+        this.errors.push('El texto es obligatorio.');
+      }
+
+      e.preventDefault();
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
